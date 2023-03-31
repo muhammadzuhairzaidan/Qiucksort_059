5d901bb3684f2c78a4413f7b39473ad65dd8f620
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-03-31 21:53:26 +0700

    mendkelarasikan array , mendeklarasikan banyak perbandingan dan mendeklarasikan banyak data bergeser

---
+++ Qiucksort/Qiucksort.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+//array of integers to hold values
+int arr[20];
+int cmp_count = 0; // number of comprasion
+int mov_count = 0; // number of data movemnts
+int n;
+
