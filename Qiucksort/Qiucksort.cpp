282bc04aca6a758abd071d13d7d430384135173c
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-03-31 22:27:10 +0700

    menampilkan  dengan menggunakan void Sorted Array

--- Qiucksort/Qiucksort.cpp
+++ Qiucksort/Qiucksort.cpp
@@ -89,4 +89,40 @@ void q_short(int low, int high) {
 	q_short(j + 1, high);							//langkah 13
 
 }
+void display() {
+	cout << "\n------------------" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+	cout << "Number of data movements: " << mov_count << endl;
+
+}
+void display() {
+	cout << "\n------------------" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+	cout << "Number of data movements: " << mov_count << endl;
+
+}
+
+
+
+
+
+
+
+
 
