3af05c54b17a723c53fbcb0d606b2ceb325fcb22
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-03-31 22:02:46 +0700

    menukar dengan void int x dan int y

--- Qiucksort/Qiucksort.cpp
+++ Qiucksort/Qiucksort.cpp
@@ -28,3 +28,15 @@ void input() {
 		cin >> arr[i];
 	}
 }
+//swaps the element at index x with the element at index y
+void swap(int x, int y)
+{
+	int temp;
+
+	temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+}
+
+
+
